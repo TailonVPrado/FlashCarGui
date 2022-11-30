64b6957f41ae005c302d1fc77c0d74a0ac330614
TailonVPrado <tailon.prado@edu.unipar.br>
2022-11-29 22:38:15 -0300

    commit inicial

---
+++ flashCarPav/src/main/java/br/unipar/flashcar/Flashcar.java
@@ -0,0 +1,12 @@
+package br.unipar.flashcar;
+
+import Interface.Menu;
+
+public class Flashcar {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setTitle("F L A S H C A R");
+        menu.setVisible(true);
+    }
+}
